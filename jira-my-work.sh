14d02765c5975e375925373418ad625b1a697460
Don Hsieh <don.hsieh@opennet.tw>
2020-12-30 17:29:15 +0800

    add go to Jira

---
+++ jira-my-work.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Go To Jira My Work
+# @raycast.mode silent
+# @raycast.author Don
+# @raycast.authorURL https://github.com/dontw
+
+# Optional parameters:
+# @raycast.icon 📘
+
+open "https://opennetltd.atlassian.net/jira/your-work"
